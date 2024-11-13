5b1994182c286f992f57b93803daca894b417aac
Martin Ingvaldsen <martin.ingvaldsen@unitxlabs.com>
2024-11-13 10:05:07 -0800

    Workshop 6: Stereo matching fundamentals

--- oaf_vision_3d/transformation_matrix.py
+++ oaf_vision_3d/transformation_matrix.py
@@ -14,23 +14,25 @@
 # - Rotate, translate, and transform points
 # - Use the `@` operator to apply the transformation to points or combine two
 #   transformations
+# - Convert to and from a dictionary
+# - Write to and read from a JSON file
 
 # %%
 from __future__ import annotations
 
+import json
 from dataclasses import dataclass, field
-from typing import TypeVar, overload
+from pathlib import Path
+from typing import overload
 
 import numpy as np
 from nptyping import Float32, NDArray, Shape
 from scipy.spatial.transform import Rotation
 
-T = TypeVar("T", NDArray[Shape["H, W, 3"], Float32], "TransformationMatrix")
-
 
 @dataclass
 class TransformationMatrix:
-    rotation: Rotation = Rotation.from_matrix(np.identity(3))
+    rotation: Rotation = Rotation.identity()
     translation: NDArray[Shape["3"], Float32] = field(
         default_factory=lambda: np.array([0, 0, 0], np.float32)
     )
@@ -81,11 +83,35 @@ class TransformationMatrix:
     @overload
     def __matmul__(self, other: TransformationMatrix) -> TransformationMatrix: ...
 
-    def __matmul__(self, other: T) -> T:
+    def __matmul__(
+        self, other: NDArray[Shape["H, W, 3"], Float32] | TransformationMatrix
+    ) -> NDArray[Shape["H, W, 3"], Float32] | TransformationMatrix:
         if isinstance(other, NDArray):
-            return self.transform(points=other)  # type: ignore
+            return self.transform(points=other)
         if isinstance(other, TransformationMatrix):
             return TransformationMatrix.from_matrix(
                 self.as_matrix() @ other.as_matrix()
             )
         raise NotImplementedError(other)
+
+    def to_dict(self) -> dict:
+        return {
+            "rotation": self.rotation.as_quat().tolist(),
+            "translation": self.translation.tolist(),
+        }
+
+    @staticmethod
+    def from_dict(data: dict) -> TransformationMatrix:
+        return TransformationMatrix(
+            rotation=Rotation.from_quat(np.array(data["rotation"], dtype=np.float32)),
+            translation=np.array(data["translation"], dtype=np.float32),
+        )
+
+    def write_to_json(self, file_path: Path) -> None:
+        with file_path.open("w", encoding="utf-8") as file:
+            json.dump(self.to_dict(), file, indent=4)
+
+    @staticmethod
+    def read_from_json(file_path: Path) -> TransformationMatrix:
+        with file_path.open("r", encoding="utf-8") as file:
+            return TransformationMatrix.from_dict(json.load(file))
